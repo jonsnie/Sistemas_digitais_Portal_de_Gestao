c40d79ff9fc9a069059f85f970a778e65728b7c9
Jonathan Canfield Snieckoski <jon.snie@JonSnieMac-3.local>
2019-01-10 08:39:31 -0200

    rev 0

--- waze/mapa.php
+++ waze/mapa.php
@@ -1,26 +1,18 @@
   <?
     session_start();
-    require_once("../libs/php/conn.php");
-    require_once("../libs/php/funcoes.php");
+    require("../libs/php/funcoes.php");
+    require("../libs/php/conn.php");
   ?>
+  <style>
+  iframe {
+      display: block;       /* iframes are inline by default */
+      background: #000;
+      border: none;         /* Reset default border */
+      height: 85vh;        /* Viewport-relative units */
+      width: 85vw;
+      margin-top: -80px;
+  }
+  </style>
   <section role="main" class="content-body">
-      <header class="page-header">
-        <h2>Mapa do Waze</h2>
-        <div class="right-wrapper pull-right" style='margin-right:15px;'>
-          <ol class="breadcrumbs">
-            <li><a href="index_sistema.php"><i class="fa fa-home"></i></a></li>
-            <li><span class='text-muted'>Waze</span></li>
-            <li><span class='text-muted'>Mapa</span></li>
-          </ol>
-        </div>
-      </header>
-      <section class="panel">
-        <header class="panel-heading">
-          <div class="panel-actions" style='margin-top:-12px'></div>
-        </header>
-        <div class="panel-body">
-          <iframe src="https://embed.waze.com/pt-BR/iframe?zoom=12&lat=-26.294688&lon=-48.848253"
-            width="1000" height="400"></iframe>
-        </div>
-      </section>
+    <iframe src="https://embed.waze.com/pt-BR/iframe?zoom=12&lat=-26.294688&lon=-48.848253"></iframe>
   </section>
